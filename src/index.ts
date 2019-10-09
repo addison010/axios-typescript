bd58739e5c271ea9a6aa6f5be97dfa83fea6ea8a
Addison Wen <perhaps0660@gmail.com>
2019-10-09 11:34:56 +1100

    feat(xhr): added transform headers
    added transform headers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildUrl } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 const axios = (config: AxiosRequestConfig): void => {
+  processConfig(config)
   xhr(config)
+}
+
+const processConfig = (config: AxiosRequestConfig): void => {
+  config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+const transformUrl = (config: AxiosRequestConfig): string => {
+  const { url, params } = config
+  return buildUrl(url, params)
+}
+
+const transformRequestData = (config: AxiosRequestConfig): any => {
+  return transformRequest(config.data)
+}
+
+const transformHeaders = (config: AxiosRequestConfig): any => {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,10 +1,18 @@
 import { AxiosRequestConfig } from './types'
 const xhr = (config: AxiosRequestConfig): void => {
-  const { url, method = 'get', data = null } = config
+  const { url, method = 'get', data = null, headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true)
+
+  Object.keys(headers).map(name => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 
   request.send(data)
 }
